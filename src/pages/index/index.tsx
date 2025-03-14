425ca82136e4ba90fa56cff70f5a5928cf7a9390
Kennytian <kt.tian@gmail.com>
2025-03-15 07:24:26 +0800

    feat: upgrade dependencies

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,5 @@
-import { View, Text } from '@tarojs/components'
+import { View,Text  } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
-import './index.scss'
 
 export default function Index() {
   useLoad(() => {
@@ -9,8 +8,10 @@ export default function Index() {
 
   return (
     <View>
-      <View className='bg-[#89ab8d] flex flex-col'>
-        <View className='text-[#438821] dark:text-[#ec4f4f] before:content-["11111"]'>Hello world!</View>
+      <View className='flex flex-col'>
+        <Text className='bg-gradient-to-r from-green-400 to-sky-400 bg-clip-text text-xl font-extrabold text-transparent underline'> weapp-vite & weapp-tailwindcss</Text>
+        <Text>abc</Text>
+        <View className='text-[#438821] dark:text-[#ec4f4f] before:content-["11111"]'>333Hello world!</View>
         <View className='text-[#3d31a4] before:content-["222"]'>Hello world!</View>
       </View>
       <View className='bg-[#89ab8d] flex divide-x-8 divide-solid divide-[#60d256]'>
